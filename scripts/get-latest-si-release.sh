b0e4482f373f97c7deb59a879b8c54f1e6c499a6
Derek Smart <derek@grindaga.com>
2022-03-21 23:51:15 -0400

    replace SI_VERSION when tag is new
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- scripts/get-latest-si-release.sh
+++ scripts/get-latest-si-release.sh
@@ -11,4 +11,8 @@ LATEST_TAG=$(curl -s -X GET -H "Authorization: token $GITHUB_PAT" \
   "https://api.github.com/repos/simple-icons/simple-icons/releases" \
   | jq -r 'first(.[].tag_name)')
 
-echo $LATEST_TAG
+CURRENT_TAG=$(cat SI_VERSION)
+
+if [[ "$CURRENT_TAG" != "$LATEST_TAG" ]]; then
+  echo $LATEST_TAG > SI_VERSION
+fi
